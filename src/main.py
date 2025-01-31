76ed1da2bf4b42f42dd88875d86ff5348a70f4ef
lheesoo1020 <lheesoo1020@gmail.com>
2025-02-01 05:44:19 +0900

    Feat: 얼굴인식 추가 (haar cascade)

--- src/main.py
+++ src/main.py
@@ -9,6 +9,17 @@ app = FastAPI()
 
 # model = TmpModel()
 
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+def detect_faces(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 5, minSize=(30, 30))
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    return frame
+
 # 스트리밍 함수
 def generate_frames():
     # 기본 카메라 사용
@@ -17,6 +28,10 @@ def generate_frames():
         success, frame = cap.read()
         if not success:
             break
+
+        # 얼굴감지
+        frame = detect_faces(frame)
+
         # 프레임을 JPEG로 인코딩
         _, buffer = cv2.imencode('.jpg', frame)
         frame_bytes = buffer.tobytes()
